7ac4b510b785c6241dd58c34a3c73dbc9a912e75
B Suraj Patra <ankitsuraj1111@gmail.com>
2024-11-13 18:05:12 +0530

    Updates the client

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import StudentList from './Components/StudentList';
 import FacultyList from './Components/FacultyList';
+import FileUpload from './Components/FileUpload'; 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="main-heading">Student Management System</h1>
-      <StudentList />
-      <h1 className="main-heading">Faculty Management System</h1>
-      <FacultyList />
-      
-      <footer className="footer">
-        &copy; 2024 - All rights reserved | Developed with ♥ by B Suraj Patra
-      </footer>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <h1 className="main-heading">CRUD MVC</h1>
+          <nav className="navbar">
+            <Link to="/students">Students</Link>
+            <Link to="/faculty">Faculty</Link>
+            <Link to="/bulk-upload">Upload</Link>
+          </nav>
+        </header>
+        
+        <main>
+          <Routes>
+            <Route path="/students" element={<StudentList />} />
+            <Route path="/faculty" element={<FacultyList />} />
+            <Route path="/bulk-upload" element={<FileUpload />} /> 
+          </Routes>
+        </main>
+
+        <footer className="footer">
+          &copy; 2024 - All rights reserved | Developed with ♥ by B Suraj Patra
+        </footer>
+      </div>
+    </Router>
   );
 }
 
